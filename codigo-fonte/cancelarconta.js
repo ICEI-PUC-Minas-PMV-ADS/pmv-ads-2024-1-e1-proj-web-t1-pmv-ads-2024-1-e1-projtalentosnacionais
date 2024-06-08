50c07e6c0b596aad005f46aadd639016e6910481
HenriqueRodriguesLelis <164432177+HenriqueRodriguesLelis@users.noreply.github.com>
2024-06-08 15:51:03 -0300

    Create cancelarconta.js

---
+++ codigo-fonte/cancelarconta.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.querySelector('form');
+    const confirmarInput = document.getElementById('confirmar');
+    console.log("ENTROU NO JAVASCRIPT");
+       form.addEventListener('submit', function(event) {
+        if (confirmarInput.value.toUpperCase() !== 'CANCELAR') {
+           alert('Por favor, digite "CANCELAR" para confirmar o cancelamento da sua conta.');
+            event.preventDefault(); 
+        } else {
+               const confirmation = confirm('Você tem certeza que deseja cancelar sua conta? Esta ação é irreversível.');
+            if (!confirmation) {
+                event.preventDefault(); 
+            } else {
+                alert('CONTA CANCELADA COM SUCESSO');
+                window.location.href =".../home/home.html"; 
+            }
+        }
+    });
+});
