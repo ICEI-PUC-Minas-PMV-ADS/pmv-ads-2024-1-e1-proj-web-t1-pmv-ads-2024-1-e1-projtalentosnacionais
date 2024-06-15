2f3c7a6c9012279aaeb8e2ea0ab27314e5f09081
HenriqueRodriguesLelis <164432177+HenriqueRodriguesLelis@users.noreply.github.com>
2024-06-15 10:57:09 -0300

    Update paginadebusca.js

--- codigo-fonte/paginadebusca.js
+++ codigo-fonte/paginadebusca.js
@@ -1,10 +1,43 @@
-function performSearch() {
-    const query = document.getElementById('search-input').value.trim(); // Obtém o valor de busca e remove espaços em branco extras
-
-    if (query !== '') { // Verifica se a consulta não está vazia
-        alert('Você buscou por: ' + query);
-        // Aqui você pode adicionar a lógica para realizar a busca real, como redirecionar para uma página de resultados ou atualizar uma seção da página com os resultados
-    } else {
-        alert('Por favor, insira um termo de busca.'); // Avisa ao usuário se a consulta estiver vazia
+function validarCampos(nome, email, username, password, confirmarSenha) {
+    if (!nome) {
+        return 'Por favor, preencha o nome completo.';
     }
+    if (!email) {
+        return 'Por favor, preencha o e-mail.';
+    }
+    if (!username) {
+        return 'Por favor, preencha o nome de usuário.';
+    }
+    if (!password) {
+        return 'Por favor, preencha a senha.';
+    }
+    if (!confirmarSenha) {
+        return 'Por favor, confirme a senha.';
+    }
+    if (password !== confirmarSenha) {
+        return 'As senhas não coincidem.';
+    }
+    return ''; 
 }
+
+function handleSubmit(event) {
+    event.preventDefault(); 
+
+    const nome = document.getElementById('Nome').value.trim();
+    const email = document.getElementById('Email').value.trim();
+    const username = document.getElementById('username').value.trim();
+    const password = document.getElementById('password').value.trim();
+    const confirmarSenha = document.getElementById('ConfirmarSenha').value.trim();
+
+
+    const mensagemErro = validarCampos(nome, email, username, password, confirmarSenha);
+    if (mensagemErro) {
+        alert(mensagemErro);
+        return;
+    }
+    alert('Cadastro realizado com sucesso!');
+    
+    window.location.href ='paginaprincipal.html'; 
+}
+
+document.querySelector('form').addEventListener('submit', handleSubmit);
