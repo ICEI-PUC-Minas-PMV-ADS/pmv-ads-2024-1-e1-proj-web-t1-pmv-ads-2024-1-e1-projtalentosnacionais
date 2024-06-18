92023434a0bddd7f55c49ee5d498cce1ec3ff632
gabygardiano <165951000+gabygardiano@users.noreply.github.com>
2024-06-18 16:27:54 -0300

    Update linksparaasredes.js

--- codigo-fonte/linksparaasredes.js
+++ codigo-fonte/linksparaasredes.js
@@ -1,28 +1,6 @@
-// Função para lidar com a inserção dos links sociais
-function inserirLinkSocial(tipo) {
-    let link = prompt("Insira o link:");
-    if (link !== null && link !== "") {
-        switch (tipo) {
-            case 'linkedin':
-                alert("Link do LinkedIn inserido: " + link);
-                break;
-            case 'facebook':
-                alert("Link do Facebook inserido: " + link);
-                break;
-            case 'instagram':
-                alert("Link do Instagram inserido: " + link);
-                break;
-            case 'lattes':
-                alert("Link do Currículo Lattes inserido: " + link);
-                break;
-            case 'email':
-                alert("Email inserido: " + link);
-                break;
-            case 'github':
-                alert("Link do Github inserido: " + link);
-                break;
-            default:
-                break;
-        }
+function insertLink(platform) {
+    let link = prompt(`Insira o link do ${platform}:`);
+    if (link) {
+        alert(`Link do ${platform} inserido: ${link}`);
     }
 }
